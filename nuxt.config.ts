06b9f5b8c1d5eaedfcc9a66c236d1cf72ff54419
vadym4che <vadym4che@gmail.com>
2023-09-20 13:37:11 +0300

    add global styles: variables, fonts, typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,36 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/7eminar-higher-accounting-landing/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
-  }
+
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: '', // Add crossorigin attribute
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Jost&family=Raleway:wght@700&display=swap',
+        },
+      ],
+    },
+
+  },
+
+  modules: ['nuxt-swiper'],
+  // swiper: {
+  //   // Swiper options
+  //   //----------------------
+  //   // prefix: 'Swiper',
+  //   // styleLang: 'css',
+  //   // modules: ['navigation', 'pagination'], // all modules are imported by default
+  // }
+
+  css: ['~/assets/css/main.css'],
 
 })
